7d61523328732b3faab0213313242329b91b1908
Federica-Fucetola <federica.fucetola@ucdconnect.ie>
2025-02-23 14:53:34 +0000

    Add files via upload

---
+++ HexOust/Players.java
@@ -0,0 +1,67 @@
+import javax.swing.*;
+import java.awt.event.*;
+
+public class Players extends JDialog {
+    private JPanel contentPane;
+    private JButton buttonOK;
+    private JButton buttonCancel;
+    private JTextField name1;
+    private JTextField name2;
+    private JComboBox color2;
+    private JComboBox color1;
+    private JLabel player1;
+    private JLabel player2;
+
+    public Players() {
+        setContentPane(contentPane);
+        setModal(true);
+        getRootPane().setDefaultButton(buttonOK);
+
+        color1.setModel(new DefaultComboBoxModel(new String[] {"Red", "Blue"}));
+        color2.setModel(new DefaultComboBoxModel(new String[] {"Blue", "Red"}));
+
+        buttonOK.addActionListener(new ActionListener() {
+            public void actionPerformed(ActionEvent e) {
+                onOK();
+            }
+        });
+
+        buttonCancel.addActionListener(new ActionListener() {
+            public void actionPerformed(ActionEvent e) {
+                onCancel();
+            }
+        });
+
+        // call onCancel() when cross is clicked
+        setDefaultCloseOperation(DO_NOTHING_ON_CLOSE);
+        addWindowListener(new WindowAdapter() {
+            public void windowClosing(WindowEvent e) {
+                onCancel();
+            }
+        });
+
+        // call onCancel() on ESCAPE
+        contentPane.registerKeyboardAction(new ActionListener() {
+            public void actionPerformed(ActionEvent e) {
+                onCancel();
+            }
+        }, KeyStroke.getKeyStroke(KeyEvent.VK_ESCAPE, 0), JComponent.WHEN_ANCESTOR_OF_FOCUSED_COMPONENT);
+    }
+
+    private void onOK() {
+        // add your code here
+        dispose();
+    }
+
+    private void onCancel() {
+        // add your code here if necessary
+        dispose();
+    }
+
+    public static void main(String[] args) {
+        Players dialog = new Players();
+        dialog.pack();
+        dialog.setVisible(true);
+        System.exit(0);
+    }
+}
