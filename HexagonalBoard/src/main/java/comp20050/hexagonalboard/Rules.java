71d2448369cf43caa9f3ef82609cba35189a859d
laauuugc <laauuugc@gmail.com>
2025-03-04 09:45:14 +0000

    Both parts running in the same project

---
+++ HexagonalBoard/src/main/java/comp20050/hexagonalboard/Rules.java
@@ -0,0 +1,53 @@
+package comp20050.hexagonalboard;
+
+import javax.swing.*;
+import java.awt.event.*;
+
+public class Rules extends JDialog {
+    private JPanel contentPane;
+    private JButton buttonOK;
+    private JTextPane rules;
+    private javax.swing.JPanel JPanel;
+
+    public Rules() {
+        setContentPane(contentPane);
+        setModal(true);
+        getRootPane().setDefaultButton(buttonOK);
+
+        buttonOK.addActionListener(e -> onOK());
+
+
+        // call onCancel() when cross is clicked
+        setDefaultCloseOperation(DO_NOTHING_ON_CLOSE);
+        addWindowListener(new WindowAdapter() {
+            public void windowClosing(WindowEvent e) {
+                onCancel();
+            }
+        });
+
+        // call onCancel() on ESCAPE
+        contentPane.registerKeyboardAction(e -> onCancel(), KeyStroke.getKeyStroke(KeyEvent.VK_ESCAPE, 0), JComponent.WHEN_ANCESTOR_OF_FOCUSED_COMPONENT);
+
+
+    }
+
+
+
+
+    private void onOK() {
+        // add your code here
+        dispose();
+    }
+
+    private void onCancel() {
+        // add your code here if necessary
+        dispose();
+    }
+
+    public static void main(String[] args) {
+        Rules dialog = new Rules();
+        dialog.pack();
+        dialog.setVisible(true);
+        System.exit(0);
+    }
+}
