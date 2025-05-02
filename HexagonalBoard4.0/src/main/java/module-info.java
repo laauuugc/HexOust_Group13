45e12ab1721a3e0ced94aff007ad4817d495e3b8
laauuugc <159537804+laauuugc@users.noreply.github.com>
2025-05-02 16:06:46 +0100

    Delete HexagonalBoard4.0 directory

--- HexagonalBoard4.0/src/main/java/module-info.java
+++
@@ -1,8 +0,0 @@
-module comp20050.hexagonalboard {
-    requires javafx.controls;
-    requires javafx.fxml;
-    requires java.desktop;
-
-    opens comp20050.hexagonalboard to javafx.fxml;
-    exports comp20050.hexagonalboard;
-}
